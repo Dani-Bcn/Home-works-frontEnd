191b79d266d19960fecd06a33d6d05ef42816b21
unknown <nneodani@gmail.com>
2022-09-25 09:43:20 +0200

    Done

--- src/components/PageChild.jsx
+++ src/components/PageChild.jsx
@@ -11,6 +11,7 @@ const [child, setChild] = useState(null);
 const [points, setPoints] = useState(0)
 const [done, setDone] = useState(true)
 const [opacity, setOpacity] = useState(1)
+const [isTasks, setIsTasks] = useState(false)
 const textCongratulations =
 [
   "Congratulations, you have achieved 30 more points !",
@@ -30,9 +31,7 @@ const handleTaskDone = async (objectTask)=>{
     showConfirmButton: false,
     timer: 1000
   })
-  // console.log(objectTask._id) 
   try{  
-    console.log(objectTask._id)  
       await axios.put(`${process.env.REACT_APP_API_URL}/child/addPoints/${id}/${objectTask._id}`)       
       setPoints(!points)        
   }catch(error){
@@ -41,61 +40,67 @@ const handleTaskDone = async (objectTask)=>{
   if(child.tasks.length === 0 ){
     setDone(false)
   }   
-} 
+}   
+
 useEffect(() => {
   const getData = async () => {
     try {      
       const getChild = await axios.get(`${process.env.REACT_APP_API_URL}/child/${id}`);                     
-       setChild(getChild.data.data)      
+       setChild(getChild.data.data)
+       if(console.log(child)){
+        console.log("occo")
+       }    
     } catch (error) { 
-      console.error(error); 
+      console.error(error);  
     } 
   }  
-  getData();  
-}, [points]);  
+  getData()
+},[])
+
 const playAnimation=(()=>{
   setOpacity(0)
-      console.log(opacity)
-})
-
+})  
 return (
   <motion.div 
   animate={{
     y:[100,0],
     opacity:[0,1]
-  }}
-  
+  }}  
   >      
-    {console.log(opacity)} 
       {child && (
         <div>      
           <motion.div className='cardPageChild'>         
             <motion.img onClick={()=>playAnimation()} src={child.imageUrl}/>        
-            <h3 >{child.name}</h3>  
+            <h3 >{child.name}</h3>             
               <pre>Points    {child.points}</pre>
               <pre>Cups        {child.cups}</pre>                   
-          </motion.div>    
-          <h4>Tasks for today</h4>  
+          </motion.div>   
+          {!isTasks && (
+            <h4> No tasks !!!</h4 >                     
+          )}
+          {isTasks && (
+            <h4>Tasks for today</h4>          
+          )}
+        
           <div className='containerListTasks'>
             {child.tasks.map(e=>{
-                  return(            
-                    <motion.div  className='cardTasks' key={e._id} onClick={()=>handleTaskDone(e)}>
-                      <h2 >{e.name}</h2>
-                      <motion.img width={100} src={e.imageUrl} alt="img task"
-                          animate={{
-                            opacity:[0,1],                            
-                          }}
-                      ></motion.img>              
-                      <h3> Points  {e.points}</h3>                                 
-                    </motion.div>                           
-                  )  
+              return(            
+                <motion.div  className='cardTasks' key={e._id} onClick={()=>handleTaskDone(e)}>
+                  <h2 >{e.name}</h2>
+                    <motion.img width={100} src={e.imageUrl} alt="img task"
+                      animate={{
+                      opacity:[0,1],                            
+                    }}
+                    ></motion.img>              
+                    <h3> Points  {e.points}</h3>                                 
+                  </motion.div>                           
+                )  
             })}         
-          </div>        
-
+          </div>    
           <NavLink to='/Sectionchilds'><button>Done</button></NavLink>
         </div>
       )}      
       {!child && <p>child not found</p>}
   </motion.div>   
-)
-} 
+)}
+ 
